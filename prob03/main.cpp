f0d5c8a60227cb4eef10241e97e949dc6c9790a2
jmurkijanian <jmurkijanian@gmail.com>
2019-09-21 14:30:45 -0700

    Wrote program that outputs a random size rectangle

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,29 @@
 // Random Rectangle
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+int main()
+{
+//Constants, and assigned values
+const int MAXHEIGHT = 3;
+const int MAX = 40;
+unsigned seed = time(0);
+srand(seed);
+int length = rand();
+
+//Gives length its max value
+//the 1 prevents it from outputing nothing
+length = rand() % MAX + 1;
+
+//outputs the # rectangle
+std::string max;
+max.assign(length,'#');
+std::cout << max << std::endl;
+std::cout << max << std::endl;
+std::cout << max << std::endl;
+
+
+return 0;
+}
